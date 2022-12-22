2e394d596e41df47251faaea75d6229ffc0d3359
andreymakeenko <mrakas1993@gmail.com>
2022-12-22 20:41:35 +0300

    add

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+// let i = 0;
+// while(i<=3){
+//     console.log(i);
+//     i++;
+// }
+// do{
+//     console.log(i);
+//     i++
+// }while(i<=3)
+// let arr = [3, 2, 5, 4, 5];
+// let result = 0;
+// for(let i=0; i<arr.length;i++){
+//    result = result + arr[i];//19
+// }
+// console.log(result);
+// && || !
+
+function save(a,b){
+    return a+b;//7
+}
+let result = save(2,5);
+console.log(result);
